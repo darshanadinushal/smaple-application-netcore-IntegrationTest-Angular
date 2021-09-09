a7a45a68b81784496d6dc4916f0e08a6661f3b89
Darshana Dinushal <darshanadinushal@gmail.com>
2021-09-10 01:27:22 +0800

    add commit

--- BackEndApplication/src/Sample.Application.RestService/Startup.cs
+++ BackEndApplication/src/Sample.Application.RestService/Startup.cs
@@ -32,6 +32,7 @@ namespace Sample.Application.RestService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<SampledbContext>(
                 options => options.UseSqlServer(this.Configuration.GetConnectionString(nameof(SampledbContext))));
             services.AddControllers();
